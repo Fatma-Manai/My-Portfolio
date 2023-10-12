b00ec5c8d18fa45da90cd7a8c0ea44320c652116
Fatma Mannai <fatma.mannai19@gmail.com>
2023-10-12 15:15:42 +0100

    save changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -103,14 +103,14 @@ function showSkills(skills) {
     });
     skillsContainer.innerHTML = skillHTML;
 }
-
+`
 function showProjects(projects) {
     let projectsContainer = document.querySelector("#work .box-container");
     let projectHTML = "";
-    projects.slice(0, 10).filter(project => project.category != "android").forEach(project => {
-        projectHTML += `
+    projects.slice(0, 10).filter(project => project.category != "").forEach(project => {
+        projectHTML +=  
         <div class="box tilt">
-      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+      <img draggable="false" src="/assets/images/${project.image}.png" alt="project" />
       <div class="content">
         <div class="tag">
         <h3>${project.name}</h3>
@@ -120,11 +120,11 @@ function showProjects(projects) {
           <div class="btns">
             <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
             <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
-          </div>
+          </div> 
         </div>
       </div>
     </div> `
-    });
+    
     projectsContainer.innerHTML = projectHTML;
 
     // < tilt js effect starts -->
@@ -134,17 +134,19 @@ function showProjects(projects) {
     // <!-- tilt js effect ends -->
 
     /* ===== SCROLL REVEAL ANIMATION ===== */
+    
     const srtop = ScrollReveal({
         origin: 'top',
         distance: '80px',
         duration: 1000,
         reset: true
     });
+    srtop.origin = 'bottom'; // Example update
+    srtop.duration = 500;    // Example update
+    /* SCROLL PROJECTS */
+    srtop.reveal('.work .box', { interval: 200 });
 
-  `  /* SCROLL PROJECTS */
-   srtop.reveal('.work .box', { interval: 200 });
 
-}
 
 fetchData().then(data => {
     showSkills(data);
@@ -157,7 +159,7 @@ fetchData("projects").then(data => {
 // <!-- tilt js effect starts -->
 VanillaTilt.init(document.querySelectorAll(".tilt"), {
     max: 15,
-});`
+});
 // <!-- tilt js effect ends -->
 
 
@@ -189,7 +191,7 @@ document.onkeydown = function (e) {
         return false;
     }
 }
-
+`
 // Start of Tawk.to Live Chat
 var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
 (function () {
@@ -200,7 +202,7 @@ var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
     s1.setAttribute('crossorigin', '*');
     s0.parentNode.insertBefore(s1, s0);
 })();
-// End of Tawk.to Live Chat
+// End of Tawk.to Live Chat `
 
 
 /* ===== SCROLL REVEAL ANIMATION ===== */
